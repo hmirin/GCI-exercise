529d762f3d607bea9984f053ab89a35a8409f74b
Tatsuaki Machida <tmsausa@gmail.com>
2017-10-16 19:59:01 +0900

    Fix main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 ### 以下を変更してください
-
+import numpy as np
 # result変数に1から1000の和を持たせてください（int型）
-result = 0
+# result = 0
+result = np.sum(np.arange(1, 1001))
 
 ### 以上を変更してください
 
